977039b0ca946ee4cd200030e332b2d75aab0c17
haxul <s.s.starodubov@yandex.ru>
2020-05-30 13:24:40 +0400

    fix service

--- src/main/java/com/pet/services/FilmService.java
+++ src/main/java/com/pet/services/FilmService.java
@@ -1,7 +1,10 @@
 package com.pet.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.pet.documents.FilmDocument;
 import com.pet.enums.FilmRemoveStatus;
+import com.pet.kafka.ConsumerFilm;
+import com.pet.kafka.ProducerFilm;
 import com.pet.models.InsertFilmRequest;
 import com.pet.repositories.FilmRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +16,15 @@ import java.util.function.Consumer;
 public class FilmService {
 
     private final FilmRepository filmRepository;
+    private final ProducerFilm producerFilm;
 
-    public FilmDocument addFilm(InsertFilmRequest request) {
+    public FilmDocument addFilm(InsertFilmRequest request) throws JsonProcessingException {
         FilmDocument filmDocument = new FilmDocument();
         filmDocument.setTitle(request.title());
         filmDocument.setDescription(request.description());
         filmDocument.setYear(request.year());
         filmRepository.save(filmDocument);
+        producerFilm.apply(filmDocument);
         return filmDocument;
     }
 
