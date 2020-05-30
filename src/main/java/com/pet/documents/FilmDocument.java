bde8ae91f3c576169ef4e0cf824ce7d96a3a272f
haxul <s.s.starodubov@yandex.ru>
2020-05-30 12:26:06 +0400

    describe document

---
+++ src/main/java/com/pet/documents/FilmDocument.java
@@ -0,0 +1,30 @@
+package com.pet.documents;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "film")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FilmDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Integer)
+    private int year;
+}
